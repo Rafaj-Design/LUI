4961b86d2c904aaf8cc9ff4ad57fc4bede3db585
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2015-05-26 16:50:10 +0100

    fixing framework

---
+++ iOS/Latest/LUIFramework.framework/Versions/A/Headers/LUIBasicData.h
@@ -0,0 +1,13 @@
+//
+//  LUIBasicData.h
+//  LUIFramework
+//
+//  Created by Ondrej Rafaj on 20/05/2015.
+//  Copyright (c) 2015 Ridiculous Innovations. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LUIBasicData : NSObject
+
+@end
