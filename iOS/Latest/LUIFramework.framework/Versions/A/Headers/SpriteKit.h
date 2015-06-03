94f258020316b95cc0bd34f1b0856845c29c0447
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2015-06-03 14:43:20 +0100

    sprite kit support

---
+++ iOS/Latest/LUIFramework.framework/Versions/A/Headers/SpriteKit.h
@@ -0,0 +1,11 @@
+//
+//  SpriteKit.h
+//
+//  Created by Ondrej Rafaj on 03/06/2015.
+//  Copyright (c) 2015 Ridiculous Innovations. All rights reserved.
+//
+
+#import <LUIFramework/LUIFramework.h>
+
+#import <LUIFramework/SKTexture+LUIImages.h>
+#import <LUIFramework/SKSpriteNode+LUIImages.h>
