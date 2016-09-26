cdd4ae7637723ef87f921072b2ac7362675e95e4
Ondrej Rafaj <ondrej.rafaj@fuerteint.com>
2016-09-26 15:01:04 +0100

    mods

--- tvOS/LUIFramework/Latest/LUIFrameworkTV.framework/Versions/A/Headers/LUIFramework.h
+++ tvOS/LUIFramework/Latest/LUIFrameworkTV.framework/Versions/A/Headers/LUIFramework.h
@@ -28,42 +28,42 @@ FOUNDATION_EXPORT const unsigned char LUIFrameworkVersionString[];
 #if TARGET_IPHONE_SIMULATOR || TARGET_OS_IPHONE
 
 // iOS only helper categories
-#import <LUIFramework/UILabel+LUITranslations.h>
-#import <LUIFramework/UIBarItem+LUITranslations.h>
-#import <LUIFramework/UISearchBar+LUITranslations.h>
-#import <LUIFramework/UITextField+LUITranslations.h>
-#import <LUIFramework/UIViewController+LUITranslations.h>
-#import <LUIFramework/UIButton+LUITranslations.h>
-#import <LUIFramework/UITextView+LUITranslations.h>
-#import <LUIFramework/UITableView+LUITranslations.h>
+#import <LUIFrameworkTV/UILabel+LUITranslations.h>
+#import <LUIFrameworkTV/UIBarItem+LUITranslations.h>
+#import <LUIFrameworkTV/UISearchBar+LUITranslations.h>
+#import <LUIFrameworkTV/UITextField+LUITranslations.h>
+#import <LUIFrameworkTV/UIViewController+LUITranslations.h>
+#import <LUIFrameworkTV/UIButton+LUITranslations.h>
+#import <LUIFrameworkTV/UITextView+LUITranslations.h>
+#import <LUIFrameworkTV/UITableView+LUITranslations.h>
 
-#import <LUIFramework/UIImageView+LUIImages.h>
-#import <LUIFramework/UIButton+LUIImages.h>
+#import <LUIFrameworkTV/UIImageView+LUIImages.h>
+#import <LUIFrameworkTV/UIButton+LUIImages.h>
 
 // iOS specific interfaces
-#import <LUIFramework/LUILanguageSelectorViewController.h>
+#import <LUIFrameworkTV/LUILanguageSelectorViewController.h>
 
 #elif TARGET_OS_MAC
 
 // Mac only helper categories
-#import <LUIFramework/NSViewController+LUITranslations.h>
-#import <LUIFramework/NSWindow+LUITranslations.h>
-#import <LUIFramework/NSButton+LUITranslations.h>
-#import <LUIFramework/NSTextField+LUITranslations.h>
+#import <LUIFrameworkTV/NSViewController+LUITranslations.h>
+#import <LUIFrameworkTV/NSWindow+LUITranslations.h>
+#import <LUIFrameworkTV/NSButton+LUITranslations.h>
+#import <LUIFrameworkTV/NSTextField+LUITranslations.h>
 
 #endif
 
 // Common helper categories
-#import <LUIFramework/NSObject+LUITranslations.h>
-#import <LUIFramework/NSObject+LUIVisuals.h>
+#import <LUIFrameworkTV/NSObject+LUITranslations.h>
+#import <LUIFrameworkTV/NSObject+LUIVisuals.h>
 
-#import <LUIFramework/LUIEnums.h>
-#import <LUIFramework/LUILanguage.h>
-#import <LUIFramework/LUIURLs.h>
+#import <LUIFrameworkTV/LUIEnums.h>
+#import <LUIFrameworkTV/LUILanguage.h>
+#import <LUIFrameworkTV/LUIURLs.h>
 
-#import <LUIFramework/LUIMain.h>
-#import <LUIFramework/LUITranslations.h>
-#import <LUIFramework/LUIVisuals.h>
+#import <LUIFrameworkTV/LUIMain.h>
+#import <LUIFrameworkTV/LUITranslations.h>
+#import <LUIFrameworkTV/LUIVisuals.h>
 
-#import <LUIFramework/LUIDebugger.h>
+#import <LUIFrameworkTV/LUIDebugger.h>
 
--- tvOS/LUIFramework/Latest/LUIFrameworkTV.framework/Versions/A/Headers/SpriteKit.h
+++ tvOS/LUIFramework/Latest/LUIFrameworkTV.framework/Versions/A/Headers/SpriteKit.h
@@ -5,8 +5,8 @@
 //  Copyright (c) 2015 Ridiculous Innovations. All rights reserved.
 //
 
-#import <LUIFramework/LUIFramework.h>
+#import <LUIFrameworkTV/LUIFramework.h>
 
-#import <LUIFramework/SKLabelNode+LUITranslations.h>
-#import <LUIFramework/SKTexture+LUIImages.h>
-#import <LUIFramework/SKSpriteNode+LUIImages.h>
+#import <LUIFrameworkTV/SKLabelNode+LUITranslations.h>
+#import <LUIFrameworkTV/SKTexture+LUIImages.h>
+#import <LUIFrameworkTV/SKSpriteNode+LUIImages.h>
